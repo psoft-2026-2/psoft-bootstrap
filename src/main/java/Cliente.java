21b908cf0a04c474005740918be7463d048140e4
Rai Izeckson <rai.izeckson.silva@ccc.ufcg.edu.br>
2026-09-16 20:51:16 -0300

    Atv 1

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,7 @@
+public class Cliente extends Pessoas {
+
+    public Cliente(String nome, Integer idade, String cpf, String telefone, String profissao) {
+        super(nome, idade, cpf, telefone, profissao);
+    }
+    
+}
