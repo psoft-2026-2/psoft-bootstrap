3090f494a3e86245cba276f0ecc69b178b49646f
claudiopintofilho <claudio.pinto.lopes.filho@ccc.ufcg.edu.br>
2026-09-16 21:04:33 -0300

    atv1.1

--- src/main/java/Bootstrap.java
+++ src/main/java/Bootstrap.java
@@ -3,5 +3,33 @@ public class Bootstrap {
     public static void main(String[] args) {
 
         System.out.println("Projeto de Software");
+
+	private List<Pessoa> pessoas = new ArrayList<>();
+
+	//CREATE
+	public void add(Pessoa pessoa){
+		pessoas.add(pessoa);
+	}
+
+	public Pessoa buscarPessoa(String cpf){
+		for(Pessoa p : pessoas){
+			if(p.getCpf().equals(cpf)){
+				return p;
+			}
+		}
+
+		return null;
+	}
+
+	//UPDATE
+	public void update(Pessoa pessoa){
+		//depende do que botar aqui
+	}
+
+	//READ
+	public List<Pessoa> listarTodos(){
+		return pessoas;
+	}
+
     }
 }
---
+++ src/main/java/Funcao.java
@@ -0,0 +1,4 @@
+public enum Funcao{
+	CLIENTE,
+	FUNCIONARIO
+}
