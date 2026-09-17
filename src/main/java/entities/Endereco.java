9ba43a1872d99257b2c8dbac245a774a6507a630
pedro <pedro.leal.e.lima@ccc.ufcg.edu.br>
2026-09-16 22:52:12 -0300

    feat: sending all done

---
+++ src/main/java/entities/Endereco.java
@@ -0,0 +1,95 @@
+package entities;
+
+public class Endereco {
+
+    private static int proximoId = 1;
+
+    private final int id;
+    private String logradouro;
+    private String numero;
+    private String cidade;
+    private String estado;
+    private String cep;
+
+    public Endereco(String logradouro, String numero, String cidade, String estado, String cep) {
+        this.id = proximoId++;
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.cep = cep;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Endereco other = (Endereco) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return logradouro + ", " + numero + " - " + cidade + "/" + estado + " - CEP " + cep;
+    }
+}
+
+
